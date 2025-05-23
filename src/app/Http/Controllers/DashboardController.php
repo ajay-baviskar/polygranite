1c43c17e24d440545145ddb79d339af668f3067a
Ajay <ajay.baviskar@orangecurrent.com>
2025-05-23 17:03:44 +0530

    made proper admin pages

--- src/app/Http/Controllers/DashboardController.php
+++ src/app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,17 +14,19 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $chartData = [
-            'earnings_monthly' => 40000,
-            'earnings_annual' => 215000,
-            'tasks_completion' => 50,
-            'pending_requests' => 18,
-            'earnings_labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            'earnings_data' => [10000, 12000, 15000, 18000, 20000, 22000, 25000, 23000, 21000, 19000, 17000, 16000],
-            'revenue_labels' => ['Direct', 'Social', 'Referral'],
-            'revenue_data' => [55, 30, 15],
-        ];
+        // $chartData = [
+        //     'earnings_monthly' => 40000,
+        //     'earnings_annual' => 215000,
+        //     'tasks_completion' => 50,
+        //     'pending_requests' => 18,
+        //     'earnings_labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        //     'earnings_data' => [10000, 12000, 15000, 18000, 20000, 22000, 25000, 23000, 21000, 19000, 17000, 16000],
+        //     'revenue_labels' => ['Direct', 'Social', 'Referral'],
+        //     'revenue_data' => [55, 30, 15],
+        // ];
 
-        return view('admin.admin', compact('chartData'));
+        // return view('admin.admin');
+         $contacts = Contact::all();
+        return view('contact.list', compact('contacts'));
     }
 }
